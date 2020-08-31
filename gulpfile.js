6da9a5d8366262332af6b60bb3128e71269744c5
Remasterer <ronitrend@gmail.com>
2020-08-31 11:57:37 +0300

    'page-offices'

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,6 @@ function browserSync() {
 function html() {
   return src(path.src.html)
     .pipe(fileInclude())
-    .pipe(webphtml())
     .pipe(dest(path.build.html))
     .pipe(browsersync.stream());
 }
@@ -129,11 +128,6 @@ function js() {
 
 function images() {
   return src(path.src.img)
-    .pipe(
-      webp({
-        quality: 70,
-      })
-    )
     .pipe(dest(path.build.img))
     .pipe(src(path.src.img))
     .pipe(
