774e34abd93642e85d0133d418efeae3a6d59005
Ivan Elin <ivan.nero1@gmail.com>
2020-09-02 16:45:16 +0300

    Fixed script for tabs

--- Oxford/js/script.js
+++ Oxford/js/script.js
@@ -226,6 +226,12 @@ $(document).ready(function () {
       $tab = $('a', $bar),
       $content = $('.js-tabs__content', $tabs),
       $activeClass = 'active';
+  $($tab[0]).addClass($activeClass);
+  var linkHref = $($tab[0]).attr('href');
+  $content.each(function () {
+    $content.removeClass($activeClass);
+    $tabs.find(linkHref).addClass($activeClass);
+  });
   $tab.each(function () {
     var $this = $(this);
     $this.on('click', function (e) {
